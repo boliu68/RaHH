21846d12730497a1242fe28d6fd6a219418b55d6
Bo Liu <6liubo8@gmail.com>
2013-10-08 23:11:44 +0800

    The CVH is initially coded. However, the test data should be formatted. The definition in test data and origin CVH is not that same.

--- cvh.m
+++ cvh.m
@@ -1,4 +1,4 @@
-function A  = cvh(X,W)
+function A  = cvh(W,varargin)
 
 %Reference : Kumar, S., & Udupa, R. (2011). Learning hash functions for cross-view similarity search. Paper presented at the Proceedings of the Twenty-Second international joint conference on Artificial Intelligence-Volume Volume Two.
 %Input: X: n dim-dimensional instances.
@@ -6,13 +6,20 @@ function A  = cvh(X,W)
 %Output: The hash function for each view/domain/task. 
 %For initialize RaHH. Can also be used as baseline.
 
-K = size(X,1);%The number of tasks.
-N = size(X,2);;%The number of instances
-dim = size(X,3)%The number of dim.
+K = nargin-1;%The number of tasks.
+N = zeros(K,1);
+dim = zeros(K,1);
 
-D_ii = zeros(n,1);
+for k = 1:K
+    
+    N(k) = size(varargin{k},1);
+    dim(k) = size(varargin{k},2);
+    
+end
+
+D_ii = zeros(N,1);
 for n = 1:N   
-    D_ii(n) = sum(W(i,:));
+    D_ii(n) = sum(W(n,:));
 end
 D = diag(D_ii);%Initialize D
 
@@ -22,16 +29,16 @@ L_ = 2*L + (K-1)*D;
 
 for k = 1:K
    
-    eval()('A',num2str(k),'=zeros[',num2str(N(k)),',',num2str(dim(k)));
-    Left = X(k)'*L_*X(k);
-    Right = X(k)'*X(k)
+    eval(['A_' num2str(k) '=zeros(' num2str(N(k)) ',' num2str(dim(k)) ');']);
+    Left = varargin{k}'*L_*varargin{k};
+    Right = varargin{k}'*varargin{k};
     
     [A_k,lambda] = eig(Left,Right);
     
     [lambda,order] = sort(diag(lambda),'descend');
     A_k = A_k(:,order);
-    
-    eval()('A',num2str(k),'=A_k');
+   
+    A{k} = A_k
 end
 
 
--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+[H,W] = function RaHH(X,R_p,R_pq,r)
 %Relation-aware Heterogeneous Hashing(RaHH)
 %Puesdo-Code
 %Author: Bo Liu
@@ -5,30 +6,14 @@
 %Reference: 
 %Ou, M., Cui, P., Wang, F., Wang, J., Zhu, W., & Yang, S. (2013). Comparing apples to oranges: a scalable solution with heterogeneous hashing. Paper presented at the Proceedings of the 19th ACM SIGKDD international conference on Knowledge discovery and data mining.
 %Rahh()
-%Input: X^p/data, R_p,intra_domain relation, R_pq inter-domain relation.
+%Input: X^p/data, R_p,intra_domain relation, R_pq inter-domain relation. r: the number of bit for each domain
 %Output: H^p: hash function, W: map function to map the hash code to another Hamming space.
-%initialize H by cross-view-hashing and w as identity matrix.
-%initialize S.
-%while the value of objective function don't converge do.
-%for each domain p do
-%	for each entity i in domain p do:
-%		calculate the gradient with respect to h_i^p
-%		update h_i^p
-%		update S
-%	end for
-%	for each domain q do:
-%		for each bit k of domain q do
-%			calculate gradients with respects to w_k^pq
-%			update w_k^pq
-%		end for
-%	end for
-%end for
-%end while.
-[H,W] = function RaHH(X,R_p,R_pq)
 
-P = size(X,1); %The number of domain
+dim = size(X,1); %The dimension of input data
 m = size(X,2); %The number of instances
+P = size(r,1)%The number of domains
 bit = [1,2] %The number of bit for each domain. Equal to r_p in origin paper
+
 
 for p = 1:P
 	%initialize H_p
